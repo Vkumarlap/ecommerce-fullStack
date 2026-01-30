5b7056bcb2988c738f0f5f1c5003ca5d1c5f9580
Vkumarlap <vangakumar8@gmail.com>
2026-01-30 17:39:42 +0530

    git commit -m "Resolve internal server error caused by missing auto-increment on Product ID"

--- ecommerce-backend-springboot/Project/src/main/java/com/Kumar/Project/Services/Productservices.java
+++ ecommerce-backend-springboot/Project/src/main/java/com/Kumar/Project/Services/Productservices.java
@@ -1,10 +1,11 @@
 package com.Kumar.Project.Services;
 
+import java.io.IOException;
 import java.util.List;
 import com.Kumar.Project.Model.Product;
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 
 import com.Kumar.Project.Repository.Productrepo;
 
@@ -19,15 +20,23 @@ public List<Product> getproducts()  {return repo.findAll();}
 
 public Product getProductByid(int id)     {return repo.findById(id).orElse(null);} //returning null is not a good idea 
 
-public void deleteproduct(int pid) 
-{  if(!repo.existsById(pid))
-    throw new RuntimeException("Product not found for id"+pid);
-    else
-    repo.deleteById(pid);}
 
- public void addproduct(Product product)   {repo.save(product);}
+ public Product addproduct(Product product,MultipartFile imageFile) throws IOException{
+    
+    product.setImageName(imageFile.getOriginalFilename()); //here we are setting image name type and data
+    product.setImageType(imageFile.getContentType());
+    product.setImageDate(imageFile.getBytes());
+   return  repo.save(product);}
 
-  public void updateproduct(Product product ){repo.save(product);}
+
+
+   // public void deleteproduct(int pid) 
+// {  if(!repo.existsById(pid))
+//     throw new RuntimeException("Product not found for id"+pid);
+//     else
+//     repo.deleteById(pid);}
+
+//   public void updateproduct(Product product ){repo.save(product);}
 
     
 }
